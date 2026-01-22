ee9b3927997b5cc09aa8c56e568bb75da4d7cb61
Joost Clevis <joost@bytewise.nl>
2026-01-22 23:54:46 +0100

    chore: wire astro image; update pages to use astro:assets Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
 export default defineConfig({
+  integrations: [image({ service: 'sharp' })],
   experimental: {
     viewTransitions: true,
   },
